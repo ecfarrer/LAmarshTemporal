a18b1651d6c87f2a0ebaafd284271941ace35098
zshribman <zshribman@tulane.edu>
2023-04-10 14:19:40 -0500

    Test script, first day using github

---
+++ Shribman.R
@@ -0,0 +1,9 @@
+# Zoë Shribman
+# Ecological Analysis
+# Test script
+
+install.packages("gitcreds")
+library(gitcreds)
+gitcreds_set() #enter your personal access token
+
+# testing adding changes to my script
