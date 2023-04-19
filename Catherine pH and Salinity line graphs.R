d2c72d8219edc098de2c005ffcfac90aba675351
cakinyi96 <cakinyi96@gmail.com>
2023-04-19 12:04:27 -0500

    Line graphs of pH and Salinity

---
+++ Catherine pH and Salinity line graphs.R
@@ -0,0 +1,32 @@
+library(dplyr)
+library(tidyr)
+library("plotrix")
+library(ggplot2)
+Phragmites <- read.csv("C:/Users/mac/Downloads/LAmarshTemporal/PhragSurvey2017to2022.csv",stringsAsFactors = T)
+Phrag_TIME <- Phragmites %>%
+  filter(!Phragmites$Site %in% c("Turtle Cove", "Fontainebleau", "LUMCON 1", "LUMCON 2"),
+         !is.na(pH))%>% mutate(Year = as.factor(Year))
+
+#pH Over Time by Transect and Site
+Phrag_pH <- Phrag_TIME %>%
+  group_by(Site, Transect, Year)%>%
+  summarise(mean = mean(pH), se = std.error(pH)) 
+
+ggplot(Phrag_pH, aes(x = Year, y = mean, color = Transect)) +
+  geom_point(stat="identity") + 
+  geom_line()+
+  geom_errorbar(aes(ymax=mean+se,ymin=mean-se),width=.25)+
+  labs(x = "Time",y="pH")+
+  facet_wrap(~ Site, scales="free")
+
+#Salinity Over Time by Transect and Site
+Phrag_Salinity <- Phrag_TIME %>%
+  group_by(Site, Transect, Year)%>%
+  summarise(mean = mean(Salinity15cmppt), se = std.error(Salinity15cmppt)) 
+
+ggplot(Phrag_Salinity, aes(x = Year, y = mean, color = Transect)) +
+  geom_point(stat="identity") + 
+  geom_line()+
+  geom_errorbar(aes(ymax=mean+se,ymin=mean-se),width=.25)+
+  labs(x = "Time",y="Salinity")+
+  facet_wrap(~ Site, scales="free")
