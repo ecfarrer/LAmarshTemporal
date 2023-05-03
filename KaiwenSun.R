971c9e1a021108c14df35e83d0409aa954c95052
Ka1wenSun <130398845+Ka1wenSun@users.noreply.github.com>
2023-05-03 13:20:38 -0600

    Up load final code
    Up load final code

--- KaiwenSun.R
+++ KaiwenSun.R
@@ -3,7 +3,6 @@ library(gitcreds)
 gitcreds_set()
 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
 
-library(gitcreds)
 library(plotrix)
 library(ggplot2)
 library(dplyr)
@@ -17,6 +16,14 @@ library(tidyr)
 
 dat <- read.csv("PhragSurvey2017to2022.csv")
 head(dat)
+
+
+tidydat <- pivot_longer(dat,names_to="Species",
+                        values_to="Abundance",Phragmites.australis:Spartina.patens)
+
+head(tidydat)
+
+tidydat$fTransect <- factor(tidydat$Transect)
 
 
 mymeans<-dat %>%
@@ -111,5 +118,39 @@ ggplot(mymeans,aes(x=Year,y=mean,color=Transect))+
   labs(x = "Year",y="Litter")+
   facet_wrap(~Site, scales="free")
 
+###############################################################################
+library(nlme)
 
- 
+options(contracts = c("contr.helmert","contr.poly"))
+
+dat <- read.csv("PhragSurvey2017to2022.csv")
+head(dat)
+
+SpatialData <- read.csv("PhragSurvey2017to2022.csv", stringsAsFactors = TRUE)
+
+SpatialData2 <- SpatialData %>%
+  filter(!SpatialData$Site %in% c('LUMCON 1', 'LUMCON 2', 'Fontainebleau', 'Turtle Cove'), !is.na(Litter))
+
+SpatialData2$fYear <- factor(SpatialData2$Year)
+
+
+#Regular
+model1 <- gls(Litter ~ Site + fYear + Transect + fYear*Transect + Site*fYear+ Site*Transect, 
+               data = SpatialData2)
+
+#temporal model
+model1TA <- lme(
+  Litter ~ Site + fYear + Transect + fYear*Transect + Site*fYear,
+  correlation = corAR1(form =~ Year|Plot),
+  random = ~1|Plot, data = SpatialData2)
+
+#Spatial model
+model1SA <- gls(
+  Litter ~ Site + fYear + Transect + fYear*Transect + Site*fYear,
+  correlation=corSpher(form = ~ Lat+Long|Year, nugget=T),
+  data = SpatialData2)
+#nugget only for spherical model
+
+anova(model1, model1TA, model1SA)
+
+anova(model1, type = "marginal")
